14795b45a0810453c8e88147cc9e4dd821ae7e89
corentin.fievet <cofievet@gmail.com>
2022-07-18 09:04:45 +0200

    [VSCode] - extract and extensions list

---
+++ VSCode/extensions.ps1
@@ -0,0 +1,9 @@
+code --install-extension dzhavat.bracket-pair-toggler
+code --install-extension EliverLara.andromeda
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension GitHub.copilot
+code --install-extension IBM.output-colorizer
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension MS-CEINTL.vscode-language-pack-fr
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension solnurkarim.html-to-css-autocompletion
